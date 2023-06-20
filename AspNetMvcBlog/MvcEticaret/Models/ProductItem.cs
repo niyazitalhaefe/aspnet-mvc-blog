d370363dea1d3827b1e6bf313dabaec1f09b11f9
Niyazi Talha Efe <niyazitalha3f3@gmail.com>
2023-06-20 15:01:05 +0300

    MvcEticaret sminde MVC Projesi oluşturuldu.
    Partial dosyaları oluşturuldu. _Layout düzenlendi.

---
+++ AspNetMvcBlog/MvcEticaret/Models/ProductItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcEticaret.Models
+{
+    public class ProductItem
+    {
+        public string title { get; set; }
+        public double startCount { get; set; }
+        public int oldPrice { get; set; }
+        public int price { get; set; }
+        public int imageUrl { get; set; }
+        public int isSale { get; set; }
+
+        //Buradan emin değilim bu IActionResult altında liste içerisinde de yapabilir değerler verip bunu düşünmek lazım.
+    }
+}
---
+++ AspNetMvcBlog/MvcEticaret/ViewComponets/ProductItemViewComponents.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcEticaret.ViewComponets
+{
+        public class ProductItemViewComponents : ViewComponent
+        {
+            public IViewComponentResult Invoke()
+            {
+                return View();// burası değişecek hata vermemesi için return view yaptım.
+            }
+        }
+    
+}
